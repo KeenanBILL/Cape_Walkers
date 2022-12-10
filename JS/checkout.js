1850d1739d32a259d8436932f6abf2b577ee720e
KeenanBILL <t19008@tsiba.ac.za>
2022-12-10 15:27:26 +0200

    Thank you for having me

--- JS/checkout.js
+++ JS/checkout.js
@@ -18,29 +18,4 @@ cart.forEach((item) => {
     }
 });
 
-// let cartLeave = [];
-
-// localStorage.setItem('cartDepart'.JSON.stringify(cartLeave));
-
-// let fruitStore = JSON.parse(localStorage.getItem('organic'));
-
-// fruitStore.forEach((item) => {
-//     console.log(fruitStore(item).type);
-//     fruitStore.innerHTML += `
-//     <div class="col-md-4">
-//                         <div class="card" style="width: 18rem;">
-//                             <img src=${item.image} class="card-img-top" alt="...">
-//                                 <div class="card-body">
-//                                     <p class="nameF">${item.fruit}</p>
-//                                     <p class="weightF">${item.weight}</p>
-//                                     <p class="priceF">${item.price}</p>
-//                                     <button class="addToCart" href="#">BUY</button>
-//                                 </div>
-//                         </div>
-//                     </div>
-//     `
-// })
-
-
-
 
